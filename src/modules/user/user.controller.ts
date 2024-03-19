ebf2444d076b8dcdf079dedf084cd14328caddab
matheusssilva991 <matheus.ssilva991@gmail.com>
2024-03-19 19:09:59 -0300

    fix: user post private to public

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -8,6 +8,7 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { Public } from '../../common/decorator/public.decorator';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserService } from './user.service';
@@ -17,6 +18,7 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('user')
+  @Public()
   async create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
